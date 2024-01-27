9b613d12120a98365546f43e9c39dffe17bc3c30
Данила Мутрисков <hlop4ik773@gmail.com>
2024-01-27 23:11:17 +0400

    Создал модели для БД для использования EntityFramework и SQLServer

---
+++ PathfinderDatabaseImplement/Models/SkillLevel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PathfinderDatabaseImplement.Models
+{
+    public class SkillLevel
+    {
+        public int Id { get; set; }
+
+        public int SkillId { get; set; }
+
+        public int Level { get; set; }
+
+        [ForeignKey("SkillLevelId")]
+        public virtual List<ProfessionSkillLevels> ProfessionSkillLevels { get; set; }
+
+        [ForeignKey("SkillLevelId")]
+        public virtual List<EmployeeSkillLevels> EmployeeSkillLevels { get; set; }
+    }
+}
